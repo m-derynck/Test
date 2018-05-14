70528384ee2e680a44eb21ccc7bbd1dd90a67075
Matt Derynck <derynckmatthew@gmail.com>
2018-05-14 17:46:27 -0400

    Added driver code to accept user input

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -12,5 +12,15 @@ public class AccountTest {
         //Display default account info.
         System.out.printf("Initial name is: %s%n%n", myAccount.getName());
 
+        //Prompt user for account name
+        System.out.println("Please enter an account name: ");
+        String accName = input.nextLine();
+
+        //Set account name to user's input
+        myAccount.setName(accName);
+        System.out.println();
+        //Print out the account name.
+        System.out.printf("Name of my account is: %s%n",myAccount.getName());
+
     }
 }
